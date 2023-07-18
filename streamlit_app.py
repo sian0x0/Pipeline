9e518bd2b035315493176498fcbfc8782b785f1b
S Bradley <sian.f.bradley@gmail.com>
2023-07-18 19:50:01 +0200

    use original secrets dict

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,8 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-def Dictify(a):
-    i = iter(a)
-    res_dct = dict(zip(i, i))
-    return res_dct
-
 def get_data():
-    credentials = Dictify(st.secrets.service_account)
+    credentials = st.secrets.service_account
     gc = gspread.service_account_from_dict(credentials)
     wks = gc.open("pipeline-data").sheet1
     dataframe = pd.DataFrame(wks.get_all_records())
@@ -17,7 +12,6 @@ def get_data():
     return dataframe
 
 graph_data = get_data()
-#print(graph_data)
 
 st.video("https://d1io3yog0oux5.cloudfront.net/_2199c089e968c5816a59e11eeedd0b5e/hostessbrands/db/871/8254/video_mp4/hostess-home.mp4")
 
