23c71e26801812c4fcecd42bcfc091f0e70da444
S Bradley <sian.f.bradley@gmail.com>
2023-07-18 20:17:10 +0200

    add img

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,5 @@ st.text("Hostess Brands Inc (TWNK) daily closing stock price (US $)")
 st.line_chart(graph_data.rename(columns={'Date':'index'}).set_index('index'))
 
 st.button('Invest!') 
-st.image(["https://www.hostesscakes.com/wp-content/uploads/2020/03/HST_TWINK_Hero_Original_Cut.png", "https://www.hostesscakes.com/wp-content/uploads/2020/03/HST_TWINK_Hero_Original_Cut.png"])
+imgt = "https://www.hostesscakes.com/wp-content/uploads/2020/03/HST_TWINK_Hero_Original_Cut.png", width=50
+st.image([imgt, imgt, imgt, imgt])
